56f3aa3bca43d451d139cf13a9f00c0f16fc74fb
Steve Silvester <steve.silvester@shiny-geek.com>
2021-02-28 18:49:09 +0000

    math library

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber, lastBall = 1;
+
+  void newBall() {
+    setState(() {
+      do {
+        ballNumber = Random().nextInt(5) + 1;
+      } while (lastBall == ballNumber);
+      lastBall = ballNumber;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: newBall,
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
